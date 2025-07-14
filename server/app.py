914e49f4e1eba207bf2f38b32feefa4387010ff7
Brady Blair <bblair321@yahoo.comm>
2025-07-14 13:27:29 -0700

    all tests are passing

--- server/app.py
+++ server/app.py
@@ -8,3 +8,18 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/contract/<int:id>')
+def contract_info(id):
+    contract = next((c for c in contracts if c["id"] == id), None)
+    if contract:
+        return contract["contract_information"], 200
+    else:
+        return "Contract not found", 404
+
+@app.route('/customer/<customer_name>')
+def customer_info(customer_name):
+    if customer_name in customers:
+        return "", 204
+    else:
+        return "Customer not found", 404
